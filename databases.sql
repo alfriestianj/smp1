ba9fc79d1db6a4307cdbd5a34dc7fd1cebd1d14d
alfriestianj <alfriestian.jodik@gmail.com>
2022-03-30 19:56:39 +0700

    Create databases.sql
    uhuy

---
+++ databases.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 100414
+ Source Host           : localhost:3306
+ Source Schema         : databases
+
+ Target Server Type    : MySQL
+ Target Server Version : 100414
+ File Encoding         : 65001
+
+ Date: 30/03/2022 19:55:19
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
